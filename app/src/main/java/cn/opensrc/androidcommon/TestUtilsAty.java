e81f831b5735536c38e55f010fa18cd6546876cf
Sharp <sharpchencn@gmail.com>
2016-08-03 17:18:16 +0800

    Initial commit

---
+++ app/src/main/java/cn/opensrc/androidcommon/TestUtilsAty.java
@@ -0,0 +1,20 @@
+package cn.opensrc.androidcommon;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Author:       sharp
+ * Created on:   8/3/16 5:05 PM
+ * Description:
+ * Revisions:
+ */
+public class TestUtilsAty extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aty_testuitls);
+    }
+}
